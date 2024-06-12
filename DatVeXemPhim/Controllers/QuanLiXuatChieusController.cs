e9d0cab6508892866802d9c4f0604a0c88bad588
ducnqqn <ducnqqn@gmail.com>
2024-06-12 10:29:28 +0700

    duc

---
+++ DatVeXemPhim/Controllers/QuanLiXuatChieusController.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DatVeXemPhim.Data;
+using DatVeXemPhim.Models;
+
+namespace DatVeXemPhim.Controllers
+{
+    public class QuanLiXuatChieusController : Controller
+    {
+        private readonly DatVeXemPhimContext _context;
+
+        public QuanLiXuatChieusController(DatVeXemPhimContext context)
+        {
+            _context = context;
+        }
+
+        // GET: QuanLiXuatChieus
+        public async Task<IActionResult> Index(string sortOrder, string searchString, string searchDay, string searchMonth, string searchYear, string currentFilter, int? pageNumber)
+        {
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewData["CurrentFilter"] = searchString;
+            ViewData["CurrentDay"] = searchDay;
+            ViewData["CurrentMonth"] = searchMonth;
+            ViewData["CurrentYear"] = searchYear;
+
+            if (searchString != null || searchDay != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            var xuatChieus = from s in _context.XuatChieu.Include(x => x.fk_Phim).Include(x => x.fk_PhongChieu)
+                             select s;
+
+            if (!String.IsNullOrEmpty(searchDay) && int.TryParse(searchDay, out int day))
+            {
+                xuatChieus = xuatChieus.Where(s => s.ngayChieu.Day == day);
+            }
+            if (!String.IsNullOrEmpty(searchMonth) && int.TryParse(searchMonth, out int month))
+            {
+                xuatChieus = xuatChieus.Where(s => s.ngayChieu.Month == month);
+            }
+            if (!String.IsNullOrEmpty(searchYear) && int.TryParse(searchYear, out int year))
+            {
+                xuatChieus = xuatChieus.Where(s => s.ngayChieu.Year == year);
+            }
+
+            switch (sortOrder)
+            {
+                case "Date":
+                    xuatChieus = xuatChieus.OrderBy(s => s.ngayChieu);
+                    break;
+                case "date_desc":
+                    xuatChieus = xuatChieus.OrderByDescending(s => s.ngayChieu);
+                    break;
+                default:
+                    xuatChieus = xuatChieus.OrderBy(s => s.ngayChieu);
+                    break;
+            }
+
+            int pageSize = 5;
+            return View(await phanTrang<XuatChieu>.CreateAsync(xuatChieus.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
+       
+
+        // GET: QuanLiXuatChieus/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var xuatChieu = await _context.XuatChieu
+                .Include(x => x.fk_Phim)
+                .Include(x => x.fk_PhongChieu)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (xuatChieu == null)
+            {
+                return NotFound();
+            }
+
+            return View(xuatChieu);
+        }
+
+        // GET: QuanLiXuatChieus/Create
+        public IActionResult Create()
+        {
+            ViewData["maPhim"] = new SelectList(_context.Phim, "id", "id");
+            ViewData["maPhong"] = new SelectList(_context.PhongChieu, "id", "id");
+            return View();
+        }
+
+        // POST: QuanLiXuatChieus/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,maPhong,maPhim,ngayChieu,gioBatDau,gioKetThuc")] XuatChieu xuatChieu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(xuatChieu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["maPhim"] = new SelectList(_context.Phim, "id", "id", xuatChieu.maPhim);
+            ViewData["maPhong"] = new SelectList(_context.PhongChieu, "id", "id", xuatChieu.maPhong);
+            return View(xuatChieu);
+        }
+
+        // GET: QuanLiXuatChieus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var xuatChieu = await _context.XuatChieu.FindAsync(id);
+            if (xuatChieu == null)
+            {
+                return NotFound();
+            }
+            ViewData["maPhim"] = new SelectList(_context.Phim, "id", "id", xuatChieu.maPhim);
+            ViewData["maPhong"] = new SelectList(_context.PhongChieu, "id", "id", xuatChieu.maPhong);
+            return View(xuatChieu);
+        }
+
+        // POST: QuanLiXuatChieus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,maPhong,maPhim,ngayChieu,gioBatDau,gioKetThuc")] XuatChieu xuatChieu)
+        {
+            if (id != xuatChieu.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(xuatChieu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!XuatChieuExists(xuatChieu.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["maPhim"] = new SelectList(_context.Phim, "id", "id", xuatChieu.maPhim);
+            ViewData["maPhong"] = new SelectList(_context.PhongChieu, "id", "id", xuatChieu.maPhong);
+            return View(xuatChieu);
+        }
+
+        // GET: QuanLiXuatChieus/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var xuatChieu = await _context.XuatChieu
+                .Include(x => x.fk_Phim)
+                .Include(x => x.fk_PhongChieu)
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (xuatChieu == null)
+            {
+                return NotFound();
+            }
+
+            return View(xuatChieu);
+        }
+
+        // POST: QuanLiXuatChieus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var xuatChieu = await _context.XuatChieu.FindAsync(id);
+            if (xuatChieu != null)
+            {
+                _context.XuatChieu.Remove(xuatChieu);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool XuatChieuExists(int id)
+        {
+            return _context.XuatChieu.Any(e => e.id == id);
+        }
+    }
+}
--- DatVeXemPhim/Models/KhachHang.cs
+++ DatVeXemPhim/Models/KhachHang.cs
@@ -1,12 +1,35 @@
-﻿namespace DatVeXemPhim.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatVeXemPhim.Models
 {
     public class KhachHang
     {
         public int id { get; set; }
+        [Required]
+        [Column("hoTen")]
+        [Display(Name = "Họ tên")]
+        [StringLength(20, MinimumLength = 2)]
         public string hoTen { get; set; }
+        [Required]
+        [Column("soDienThoai")]
+        [Display(Name = "Số điện thoại")]
+        [StringLength(11, MinimumLength = 10)]
         public string soDienThoai { get; set; }
+        [Required]
+        [Column("email")]
+        [Display(Name = "Email")]
+        [StringLength(20, MinimumLength = 10)]
         public string email { get; set; }
+        [Required]
+        [Column("taiKhoan")]
+        [Display(Name = "Tài khoản")]
+        [StringLength(20, MinimumLength = 2)]
         public string taiKhoan { get; set; }
+        [Required]
+        [Column("matKhau")]
+        [Display(Name = "Mật khẩu")]
+        [StringLength(20, MinimumLength = 2)]
         public string matKhau { get; set; }
         public ICollection<Ve>? Ves { get; set; }
     }
